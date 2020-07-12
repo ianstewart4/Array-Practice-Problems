3b0a4bd682e13fc2529cd9d1b6c15ac7f92da8c9
Ian Stewart <ianstewart4@outlook.com>
2020-07-11 19:16:53 -0500

    adding solutions

---
+++ solutions.js
@@ -0,0 +1,53 @@
+// restarting to have them all in one place
+console.log("CONNECTED");
+
+function printReverse(arr) {
+    for (var i = arr.length - 1; i >= 0; i--) {
+        console.log(arr[i]);
+    }
+}
+printReverse([3, 6, 2, 5]);
+
+//*** isUniform()  */
+
+function isUniform(arr) {
+    var first = arr[0];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] !== first) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// *** sumArray() ***
+
+function sumArray(arr) {
+    var total = 0;
+    arr.forEach(element => {
+        total += element;
+    });
+    return total;
+}
+
+// *** max() ***
+
+// function max(arr) {
+//     var max = arr[0];
+//     arr.forEach(element => {
+//         if (element > max) {
+//             max = element;
+//         }
+//     });
+//     return max;
+// }
+
+function max(arr) {
+    var max = arr[0];
+    for(var i = 1; i < arr.length; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
